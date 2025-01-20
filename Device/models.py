f2e0b37759545e18cf80c68aba6604c19f65fbdc
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-01-20 18:52:09 -0300

    feat: configure device models, serializer and some views

--- Device/models.py
+++ Device/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    device_type = models.ForeignKey('DeviceType', on_delete=models.CASCADE)
+    mac_address = models.CharField(max_length=17)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.device_type.name}: {self.name}"
+
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=100)
--- Device/views.py
+++ Device/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Device
+from .serializer import DeviceSerializer
