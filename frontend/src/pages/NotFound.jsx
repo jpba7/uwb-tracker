a3f95ccc50edba2ed79ce104ba16b53843c615ec
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-01-30 11:16:20 -0300

    refactor: remove frontend files to rebuild from scratch

--- frontend/src/pages/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-function NotFound(){
-    return <div>
-        <h1>404 Not Found</h1>
-        <p>The page you are looking for doesn't exist!</p>
-    </div>
-        
-}
-
-export default NotFound
