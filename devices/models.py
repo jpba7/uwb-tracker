0600bbf0a50ecd3003ba5003b1916b72afedd308
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-02-08 15:48:43 -0300

    feat: create devicedatapoint model

--- devices/models.py
+++ devices/models.py
@@ -8,8 +8,18 @@ class Device(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.device_type.name}: {self.name}"
+        return f'{self.device_type.name}: {self.name}'
 
 
 class DeviceType(models.Model):
     name = models.CharField(max_length=100)
+
+
+class DeviceDataPoints(models.Model):
+    device = models.ForeignKey('Device', on_delete=models.CASCADE)
+    measure = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Device Data Point'
+        verbose_name_plural = 'Device Data Points'
