fc077a909f23a570dbd242c7695ea665ecf09a5d
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-02-08 15:50:10 -0300

    fix: webpack.config otimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -10,16 +9,6 @@ module.exports = {
     filename: '[name]-bundle.js',
     path: path.resolve(__dirname, './frontend/static-local/'),
   },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30
-    }
-  },
   module: {
     rules: [
       {
@@ -27,7 +16,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
-      }
+      },
     ]
   }
-}
+};
