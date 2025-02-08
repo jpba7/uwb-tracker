62b5b008a7fb07f65a612ffc53011eac27934bdf
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-02-08 16:14:32 -0300

    feat: get chart data from django instead of static

---
+++ frontend/graphs/LineChart.js
@@ -0,0 +1,93 @@
+import ReactECharts from 'echarts-for-react'
+import * as echarts from 'echarts'
+
+const LineChart = ({x_data, y_data}) => {
+
+    const option = {
+        color: ['var(--orange)'],
+
+        toolbox: {
+            feature: {
+                saveAsImage: {},
+            }
+        },
+
+        tooltip: {
+            trigger: "axis",
+            axisPointer: {
+                type: "cross"
+            },
+            backgroundColor: "rgba(0, 0, 0, 0.59)",
+            borderWidth: 0,
+        },
+        grid: {
+            left: "3%",
+            right: "4%",
+            bottom: "3%",
+            containLabel: true,
+            show: false,
+        },
+
+        xAxis: [
+            {
+                type: "category",
+                boundaryGap: false,
+                data: x_data
+            }
+        ],
+        yAxis: [
+            {
+                type: "value",
+                splitLine: {
+                    show: false,
+                }
+            }
+        ],
+        series: [
+            {
+                type: "line",
+                smooth: true,
+                lineStyle: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                        {
+                            offset: 0,
+                            color: "rgb(255, 191, 0)",
+                        },
+                        {
+                            offset: 1,
+                            color: "#F450D3"
+                        }
+                    ]),
+                    width: 4
+                },
+                areaStyle: {
+                    opacity: .5,
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 0.8, [
+                        {
+                            offset: 0,
+                            color: "#FE4C00"
+                        },
+                        {
+                            offset: 1,
+                            color: "rgba(255,144,70,0.1)"
+                        }
+                    ])
+                },
+                emphasis: {
+                    focus: "series",
+                },
+                showSymbol: false,
+                data: y_data
+            }
+        ]
+    }
+
+    return (
+        <ReactECharts 
+            option={option}
+            opts={{ renderer: 'canvas' }}
+        />
+    )
+}
+
+export default LineChart
