3f3b175d796a5c09b90545c1c6bcadc0bc821030
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-02-10 13:09:39 -0300

    feat: add tag model

--- devices/models.py
+++ devices/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from employees.models import Employee
 
 
 class Device(models.Model):
@@ -23,3 +24,19 @@ class DeviceDataPoints(models.Model):
     class Meta:
         verbose_name = 'Device Data Point'
         verbose_name_plural = 'Device Data Points'
+
+
+class Tag(Device):
+    linked_employee = models.ForeignKey('employees.Employee', on_delete=models.CASCADE)
+    total_distance = models.DecimalField(max_digits=10, decimal_places=2)
+    atual_employee_distance = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name} - {self.linked_employee}'
+
+    def assign_employee_by_cpf(self, cpf):
+        employee = Employee.objects.get(cpf=cpf)
+        self.linked_employee = employee
+        self.atual_employee_distance = 0
+        self.save()
+        return self.linked_employee
