5f6eb9f38947b5a84a888e6d03eec117dd1e35ec
João Pedro Bimbato Araujo <95439851+jpba7@users.noreply.github.com>
2025-02-12 22:37:27 -0300

    chore: remove unused files

--- frontend/django-variables.js
+++
@@ -1,9 +0,0 @@
-export const DJANGO_URLS = {
-    INDEX: "{% url 'index' %}",
-    LOGIN: "{% url 'login %}",
-    PASSWORD_RESET: "{% url 'password_reset' %}"
-};
-
-export const EMAILS = {
-    MANAGER: "joao.bimbato@molamola.com.br"
-}
