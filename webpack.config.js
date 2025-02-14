ce48ec367ebf14d0f11965d7c83f141a2d0a213c
João Pedro Bimbato Araujo <nhcsfl@gmail.com>
2025-02-14 09:48:28 -0300

    chore: remove weird minization

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,7 +7,8 @@ module.exports = {
   },
   output: {
     filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, './frontend/static-local/'),
+    path: path.resolve(__dirname, './frontend/static-local'),
+    clean: true
   },
   module: {
     rules: [
@@ -19,29 +19,5 @@ module.exports = {
         options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
       },
     ]
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: 1, // Reduz uso de CPU
-        terserOptions: {
-          compress: {
-            drop_console: true
-          }
-        }
-      })
-    ],
-    splitChunks: {
-      chunks: 'all',
-      minSize: 20000,
-      maxSize: 244000, // ~240KB por chunk
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30
-    }
-  },
-  performance: {
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000
   }
 };
