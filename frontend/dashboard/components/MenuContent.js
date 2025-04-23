2c92b710f91f38489cf7413b60c5e153a41251b2
João Pedro Bimbato Araujo <95439851+jpba7@users.noreply.github.com>
2025-04-23 16:37:16 -0300

    feat: side menu with sublists

--- frontend/dashboard/components/MenuContent.js
+++ frontend/dashboard/components/MenuContent.js
@@ -8,13 +8,15 @@ import Stack from '@mui/material/Stack';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import PeopleRoundedIcon from '@mui/icons-material/PeopleRounded';
 import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import { TapAndPlayOutlined } from '@mui/icons-material';
 import HistoryIcon from '@mui/icons-material/History';
 import { useUser } from '../../contexts/UserContext';
+import Collapse from '@mui/material/Collapse';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ChevronRight from '@mui/icons-material/ChevronRight';
 
 const mainListItems = [
   { text: 'Início', icon: <HomeRoundedIcon />, link: '/'},
@@ -22,6 +24,17 @@ const mainListItems = [
   { text: 'Dispositivos', icon: <TapAndPlayOutlined />, link: '/devices'},
   { text: 'Histórico de Uso', icon: <HistoryIcon />, link: '/device-histories'},
   { text: 'Heatmap Geral', icon: <MapOutlinedIcon />, link: '/heatmap'},
+  { 
+    text: 'Configurações', 
+    icon: <SettingsRoundedIcon />,
+    submenu: [
+      { 
+        text: 'Mapa e limites', 
+        icon: <MapOutlinedIcon />, 
+        link: '/map-settings'
+      }
+    ]
+  }
 ];
 
 const secondaryListItems = [
@@ -34,17 +47,94 @@ const secondaryListItems = [
 
 export default function MenuContent() {
   const { user } = useUser();
+  const location = useLocation();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
       <List dense>
         {mainListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
-            <ListItemButton selected={location.pathname === item.link} component={Link} to={item.link}>
-              <ListItemIcon>{item.icon}</ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
-          </ListItem>
+          <React.Fragment key={index}>
+            {item.submenu ? (
+              <>
+                <ListItem disablePadding>
+                  <ListItemButton
+                    onClick={handleMenuOpen}
+                    selected={item.submenu.some(sub => location.pathname === sub.link)}
+                  >
+                    <ListItemIcon>{item.icon}</ListItemIcon>
+                    <ListItemText primary={item.text} />
+                    <ChevronRight 
+                      sx={{ 
+                        transform: Boolean(anchorEl) ? 'rotate(180deg)' : 'none',
+                        transition: '0.2s'
+                      }} 
+                    />
+                  </ListItemButton>
+                </ListItem>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={Boolean(anchorEl)}
+                  onClose={handleMenuClose}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                >
+                  {item.submenu.map((subItem, subIndex) => (
+                    <MenuItem
+                      key={subIndex}
+                      component={Link}
+                      to={subItem.link}
+                      onClick={handleMenuClose}
+                      selected={location.pathname === subItem.link}
+                      sx={{
+                        minHeight: 32,
+                        mr: 0.5,
+                        '& .MuiListItemIcon-root': {
+                          mr: 0.5,
+                          minWidth: 32
+                        }
+                      }}
+                    >
+                      <ListItemIcon>{subItem.icon}</ListItemIcon>
+                      <ListItemText 
+                        primary={subItem.text}
+                        sx={{ 
+                          '& .MuiListItemText-primary': {
+                            fontSize: '0.875rem'
+                          }
+                        }}
+                      />
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            ) : (
+              <ListItem disablePadding>
+                <ListItemButton
+                  selected={location.pathname === item.link}
+                  component={Link}
+                  to={item.link}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            )}
+          </React.Fragment>
         ))}
       </List>
       <List dense>
